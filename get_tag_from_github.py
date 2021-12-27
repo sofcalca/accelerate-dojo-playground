09987e45f7478a97ec09914e8dde127020be3f28
Thierry Gonard <goth@octo.com>
2021-12-27 14:56:09 +0100

    Calcule change failure rate

--- get_tag_from_github.py
+++ get_tag_from_github.py
@@ -18,13 +18,27 @@ def execute_query(query: str):
         "https://api.github.com/graphql", json={"query": query}, headers=headers
     )
     response.raise_for_status()
-    response_json = response.json()
-    print(response_json)
+    return response.json()
+
+
+def get_all_tag_names(response) -> list:
+    return [
+        edge["tag"]["name"] for edge in response["data"]["repository"]["tags"]["edges"]
+    ]
+
+
+def compute_change_failure_rate(production_tags: list) -> float:
+    hotfix_tags = [tag for tag in production_tags if not tag.endswith("0")]
+    return len(hotfix_tags) / len(production_tags)
 
 
 def main():
     query = get_query_from_file("graphql_tag_query.txt")
-    execute_query(query)
+    response = execute_query(query)
+    tags = get_all_tag_names(response)
+    production_tags = [tag for tag in tags if "p" in tag]
+    change_failure_rate = compute_change_failure_rate(production_tags)
+    print(change_failure_rate)
 
 
 if __name__ == "__main__":
