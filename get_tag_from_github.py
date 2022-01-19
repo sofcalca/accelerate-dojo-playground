5379f0646222556dccd4fb45ff9ec990fe7a5afd
Thierry Gonard <goth@octo.com>
2022-01-19 14:01:35 +0100

    Calcule la fréquence de déploiement

--- get_tag_from_github.py
+++ get_tag_from_github.py
@@ -1,7 +1,12 @@
 import os
+from datetime import datetime, timedelta
+from statistics import mean
+from typing import List, NamedTuple
 
 import requests
 from dotenv import load_dotenv
+
+Tag = NamedTuple("Tag", [("name", str), ("datetime", datetime)])
 
 
 def get_query_from_file(path: str) -> str:
@@ -21,24 +26,50 @@ def execute_query(query: str):
     return response.json()
 
 
-def get_all_tag_names(response) -> list:
+def get_all_tags(response) -> List[Tag]:
     return [
-        edge["tag"]["name"] for edge in response["data"]["repository"]["tags"]["edges"]
+        Tag(
+            edge["tag"]["name"],
+            datetime.strptime(
+                edge["tag"]["target"]["tagger"]["date"], "%Y-%m-%dT%H:%M:%S%z"
+            ),
+        )
+        for edge in response["data"]["repository"]["tags"]["edges"]
     ]
 
 
-def compute_change_failure_rate(production_tags: list) -> float:
-    hotfix_tags = [tag for tag in production_tags if not tag.endswith("0")]
+def retrieve_production_tags(tags: List[Tag]) -> List[Tag]:
+    return [tag for tag in tags if "p" in tag.name]
+
+
+def compute_change_failure_rate(tags: List[Tag]) -> float:
+    production_tags = retrieve_production_tags(tags)
+    hotfix_tags = [tag for tag in production_tags if not tag.name.endswith("0")]
     return len(hotfix_tags) / len(production_tags)
+
+
+def compute_deployment_frequency(tags: List[Tag]) -> timedelta:
+    production_tags = retrieve_production_tags(tags)
+    production_tags.reverse()
+
+    span_between_deployments = [
+        (tag.datetime - previous_tag.datetime).total_seconds()
+        for previous_tag, tag in zip(production_tags, production_tags[1:])
+    ]
+
+    return timedelta(seconds=mean(span_between_deployments))
 
 
 def main():
     query = get_query_from_file("graphql_tag_query.txt")
     response = execute_query(query)
-    tags = get_all_tag_names(response)
-    production_tags = [tag for tag in tags if "p" in tag]
-    change_failure_rate = compute_change_failure_rate(production_tags)
-    print(change_failure_rate)
+    tags = get_all_tags(response)
+
+    change_failure_rate = compute_change_failure_rate(tags)
+    print(f"{change_failure_rate = }")
+
+    deployment_frequency = compute_deployment_frequency(tags)
+    print(f"{deployment_frequency = !s}")
 
 
 if __name__ == "__main__":
