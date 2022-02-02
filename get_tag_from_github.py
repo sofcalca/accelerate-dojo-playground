3d8d4a6b78d3cbc7403d3029a6a12124ad29fb5c
Thierry Gonard <goth@octo.com>
2022-02-02 12:25:08 +0100

    Calcule le lead time en recherchant les commits du dépôt

--- get_tag_from_github.py
+++ get_tag_from_github.py
@@ -7,6 +7,7 @@ import requests
 from dotenv import load_dotenv
 
 Tag = NamedTuple("Tag", [("name", str), ("datetime", datetime)])
+Commit = NamedTuple("Commit", [("sha", str), ("datetime", datetime)])
 
 
 def get_query_from_file(path: str) -> str:
@@ -38,6 +39,18 @@ def get_all_tags(response) -> List[Tag]:
     ]
 
 
+def get_all_commits(response) -> List[Commit]:
+    return [
+        Commit(
+            edge["node"]["oid"],
+            datetime.strptime(edge["node"]["committedDate"], "%Y-%m-%dT%H:%M:%S%z"),
+        )
+        for edge in response["data"]["repository"]["defaultBranchRef"]["target"][
+            "history"
+        ]["edges"]
+    ]
+
+
 def retrieve_production_tags(tags: List[Tag]) -> List[Tag]:
     return [tag for tag in tags if "p" in tag.name]
 
@@ -60,16 +73,40 @@ def compute_deployment_frequency(tags: List[Tag]) -> timedelta:
     return timedelta(seconds=mean(span_between_deployments))
 
 
+def compute_lead_time(commits: List[Commit], tags: List[Tag]) -> timedelta:
+    production_tags = retrieve_production_tags(tags)
+    production_tags.reverse()
+    commits_already_processed = []
+    delta_sum = 0
+
+    for tag in production_tags:
+        tag_date = tag.datetime
+
+        for commit in commits:
+            if commit.datetime < tag_date and commit not in commits_already_processed:
+                delta_sum += (tag_date - commit.datetime).total_seconds()
+                commits_already_processed.append(commit)
+
+    return timedelta(seconds=delta_sum / len(commits))
+
+
 def main():
-    query = get_query_from_file("graphql_tag_query.txt")
-    response = execute_query(query)
-    tags = get_all_tags(response)
+    query_tags = get_query_from_file("graphql_tag_query.txt")
+    response_tags = execute_query(query_tags)
+    tags = get_all_tags(response_tags)
 
     change_failure_rate = compute_change_failure_rate(tags)
     print(f"{change_failure_rate = }")
 
     deployment_frequency = compute_deployment_frequency(tags)
     print(f"{deployment_frequency = !s}")
+
+    query_commits = get_query_from_file("graphql_commits_query.txt")
+    response_commits = execute_query(query_commits)
+    commits = get_all_commits(response_commits)
+
+    lead_time = compute_lead_time(commits, tags)
+    print(f"{lead_time = !s}")
 
 
 if __name__ == "__main__":
