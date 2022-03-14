7b226b5a7e9ba7f2cde56392387c25b525932506
Thierry Gonard <goth@octo.com>
2022-03-14 13:50:19 +0100

    Full javascript boilerplate

--- main.js
+++ main.js
@@ -23,5 +23,38 @@ async function runQuery(query) {
     }
 }
 
-const query = readFile('graphql_tag_query.txt');
-runQuery(query).then((response) => console.log(response));
+function parse_response_for_tags(response) {
+    console.error('not implemented yet');
+}
+
+function parse_response_for_commits(response){
+    console.error('not implemented yet');
+}
+
+function compute_deployment_frequency() {
+    console.error('not implemented yet');
+}
+
+function compute_change_failure_rate() {
+    console.error('not implemented yet');
+}
+
+function compute_mean_time_to_repair() {
+    console.error('not implemented yet');
+}
+
+function compute_lead_time() {
+    console.error('not implemented yet');
+}
+
+async function main() {
+    const tagsQuery = readFile('graphql_tag_query.txt');
+    const tagsResponse = await runQuery(tagsQuery);
+    console.log(tagsResponse);
+
+    const commitsQuery = readFile('graphql_commits_query.txt');
+    const commitsResponse = await runQuery(commitsQuery);
+    console.log(commitsResponse);
+}
+
+main()
